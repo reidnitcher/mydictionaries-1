2bcfedccd98bd3b3060681bedf8bef0c8673006d
Reid Nitcher <reid_nitcher1@baylor.edu>
2022-02-09 15:23:49 -0600

    hw

--- 1_dictionary start file.py
+++ 1_dictionary start file.py
@@ -1,3 +1,8 @@
+from platform import python_revision
+
+from numpy import delete
+
+
 phonebook = {'Chris':'555−1111',
              'Katie':'555−2222',
              'Joanne':'555−3333'}
@@ -6,9 +11,17 @@ print()
 print('*****  start section 1 - print dictionary ********')
 print()
 
+print(phonebook)    
+print(type(phonebook))
 
+phone = phonebook['Chris']
+print(phone)
 
+print(len(phonebook))
 
+mydictionary = dict(m=8,n=9)
+
+print(mydictionary)
 
 print()
 print('*****  end section 1 ********')
@@ -17,15 +30,17 @@ print()
 
 
 
-
-
-
-
 print()
 print('*****  start section 2 - search dictionary ********')
 print()
 
+name = 'Chris'
 
+if name in phonebook:
+    print(phonebook[name])
+
+else:
+    print(name, 'is not in the phonebook')
 
 
 
@@ -39,12 +54,15 @@ print()
 
 
 
-
-print()
+#print()
 print('*****  start section 3 - edit/append dictionary ********')
-print()
+#print()
 
+print(phonebook)
+phonebook['chris'] = '555-0123'
 
+phonebook['Chris'] = '555-4444'
+print(phonebook)
 
 
 
@@ -54,16 +72,14 @@ print()
 
 
 
-
-
-
+"""
 print()
 print('*****  start section 4 - delete/remove from dictionary ********')
 print()
 
+delete(phonebook["Chris"])
 
-
-
+"""
 
 print()
 print('*****  end section 4 ********')
@@ -71,14 +87,13 @@ print()
 
 
 
-
-
-
 print()
 print('*****  start section 5 - iterate through keys ********')
 print()
 
-
+for key in phonebook: 
+    print(key)
+    print(phonebook[key])
 
 
 print()
@@ -86,15 +101,12 @@ print('*****  end section 5 ********')
 print()
 
 
-
-
-
-
 print()
 print('*****  start section 6 - iterate through values  ********')
 print()
 
-
+for value in phonebook.values():
+    print(value)
 
 
 print()
@@ -102,18 +114,17 @@ print('*****  end section 6 ********')
 print()
 
 
-
-
-
-
-
-
 print()
 print('*****  start section 7 - iterate through both key and value pair********')
 print()
 
 
+for phonebook_tuple in phonebook.items():
+    print(phonebook_tuple)
 
+for key,value in  phonebook.items():
+    print(key)
+    print(value)
 
 
 print()
@@ -121,21 +132,57 @@ print('*****  end section 7 ********')
 print()
 
 
-
-
-
-
-
-
 print()
-print('*****  start section 8 - using random and converting to list ********')
+print('*****  start section 8 - using get and clear ********')
 print()
 
 
+phone = phonebook.get('Chris', 'key not found')
+print(phone)
+
+phonebook.clear()
+print(phonebook)
+
+print()
+print('*****  end section 8  ********')
+print()
+"""
+print()
+print('*****  start section 9 - using pop method ********')
+print()
+
+remove = phonebook.pop('Chris', 'not found')
+print(remove)
+
+print(phonebook)
+print('*****  end section 9 ********')
+print()
+
+print()
+print('*****  start section 10 - using popitem ********')
+print()
+
+a = phonebook.popitem()
+print(a)
+print(phonebook)
+
+print()
+print('*****  end section 10 ********')
+print()
+"""
+print()
+print('*****  start section 11 - using random and converting to list ********')
+print()
+
+import random
+
+phone = phonebook[random.choice(list(phonebook))]
+
+print(phone)
 
 
 print()
-print('*****  end section 8 ********')
+print('*****  end section 11 ********')
 print()
 
 
